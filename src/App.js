9226291fb55f073ee453a26ebb5da44881bbe98b
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-14 21:42:46 +0800

    applied usestate on select input

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function Form() {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 🤔</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
