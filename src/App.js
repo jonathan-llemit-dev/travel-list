940e3d6da2bf5a357cfe627a0e3a120ccd04d95c
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-21 11:14:45 +0800

    onToggleItem utilized

--- src/App.js
+++ src/App.js
@@ -101,7 +101,13 @@ function PackingList({ items, onRemoveItem, onToggleItem }) {
 function Item({ item, onRemoveItem, onToggleItem }) {
   return (
     <li>
-      <input type="checkbox" value={item.packed} />
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
