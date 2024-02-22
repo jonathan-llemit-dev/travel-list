0ca3d46f79bd5a1fe3fe9795dd918ee42de46149
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-22 18:24:04 +0800

    added clear list button

--- src/App.js
+++ src/App.js
@@ -118,6 +118,7 @@ function PackingList({ items, onRemoveItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button>Clear list</button>
       </div>
     </div>
   );
