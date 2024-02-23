4fe274504d4653637282ab4240989f544fcf47ae
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-23 17:46:54 +0800

    Form component moved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Logo from "./Logo";
+import Form from "./Form";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -45,46 +46,6 @@ export default function App() {
       />
       <Stats items={items} />
     </div>
-  );
-}
-
-function Form({ onAddItem }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!description) return;
-
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-    onAddItem(newItem);
-
-    setDescription("");
-    setQuantity(1);
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip? 🤔</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button>Add</button>
-    </form>
   );
 }
 
